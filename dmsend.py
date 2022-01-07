a01c7180045ac2a937fb19d32d6321643c9b887f
lee98131 <67247665+lee98131@users.noreply.github.com>
2022-01-07 14:06:38 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -1,35 +1,36 @@
 import discord
-import asyncio
-import datetime
 
 client = discord.Client()
 
-__TOKEN__ = '★봇토큰★'
-titlke = '★임베드 제목 입력★'
+
+token = 'ODc3MTUwNTUyODkzNTcxMTMy.YRucHA.xwLBKEDQSDqe3aNh1oCj1rJ1A4M'
+
 
 @client.event
 async def on_ready():
-    print(client.user)
-    #game = discord.Game('상태메시지')
-    #await client.change_presence(status=discord.Status.online, activity=game)
+    print("봇이 성공적으로 실행되었습니다.")
+    game = discord.Game('부엉이 키우기')
+    await client.change_presence(status=discord.Status.online, activity=game)
 
-#!디엠 {할말}
+
 @client.event
 async def on_message(message):
+    if message.guild is None:
+        if message.author.bot:
+            return
+        else:
+            embed = discord.Embed(colour=discord.Colour.blue(), timestamp=message.created_at)
+            embed.add_field(name='전송자', value=message.author, inline=True)
+            embed.add_field(name='반갑노', value=message.content, inline=True)
+            embed.set_footer(text=f'!디엠 <@{message.author.id}> [할말] 을 통해 답장을 보내주세요!')
+            await client.get_channel(865076982303424522).send(f"`{message.author.name}({message.author.id})`", embed=embed)
+
     if message.content.startswith('!디엠'):
-        for i in message.guild.members:
-            if i.bot:
-                pass
-            else:
-                try:
-                    msg = message.content[4:]
-                    if message.author.guild_permissions.manage_messages:
-                        embed = discord.Embed(title=tile, description=msg, colour=discord.Colour.gold(), timestamp=message.created_at)
-                        #embed.set_footer(text="맨 밑에 들어갈 내용")
-                        await i.send(embed=embed)
-                        print(f'{i} 전송')
-                except:
-                    pass
-
-
-client.run(__TOKEN__)
+        if message.author.guild_permissions.manage_messages:
+            msg = message.content[26:]
+            await message.mentions[0].send(f"**{message.author.name}** 님의 답장: {msg}")
+            await message.channel.send(f'`{message.mentions[0]}`에게 DM을 보냈습니다')
+        else:
+            return
+        
+client.run(token)
